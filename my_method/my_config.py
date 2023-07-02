5d9f368507914dcd83434907047720dc237ca6f3
riley739 <rbehlevanas@gmail.com>
2023-07-02 21:04:18 +1000

    add generic base pipelines

--- my_method/my_config.py
+++ my_method/my_config.py
@@ -1,22 +1,20 @@
 from nerfstudio.engine.trainer import TrainerConfig
 from nerfstudio.plugins.types import MethodSpecification
-from my_method.pipeline import MyPipeline
-from nerfstudio.pipelines.base_pipeline import (
-    VanillaPipeline,
-    VanillaPipelineConfig,
-)
-
+from nerfstudio.cameras.camera_optimizers import CameraOptimizerConfig
 from nerfstudio.configs.base_config import ViewerConfig
 
-from nerfstudio.data.datamanagers.base_datamanager import VanillaDataManager, VanillaDataManagerConfig
+from nerfstudio.engine.optimizers import AdamOptimizerConfig, RAdamOptimizerConfig
+from nerfstudio.engine.schedulers import (
+    ExponentialDecaySchedulerConfig,
+)
 
 from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig
-from nerfstudio.engine.optimizers import AdamOptimizerConfig, RAdamOptimizerConfig
+from nerfstudio.engine.optimizers import RAdamOptimizerConfig
 from nerfstudio.engine.trainer import TrainerConfig
-from nerfstudio.models.mipnerf import MipNerfModel
-from nerfstudio.models.vanilla_nerf import NeRFModel, VanillaModelConfig
-from nerfstudio.models.nerfacto import NerfactoModel
-from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
+
+from my_method.my_dataManager import MyDataManagerConfig
+from my_method.my_nerf import MyModelConfig
+from my_method.my_pipeline import MyPipeline
 
 MyMethod = MethodSpecification(
     config=TrainerConfig(
@@ -26,23 +24,41 @@ MyMethod = MethodSpecification(
 	    max_num_iterations=30000,
 	    mixed_precision=True,
 	    pipeline=MyPipeline(
-	        datamanager=VanillaDataManagerConfig(
+	        datamanager=MyDataManagerConfig(
 	            dataparser=NerfstudioDataParserConfig(),
-	            train_num_rays_per_batch=1024
+	            train_num_rays_per_batch=1024,
+                eval_num_rays_per_batch=1024,
+                camera_optimizer=CameraOptimizerConfig(
+					mode="SO3xR3",
+					optimizer=RAdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-3),
+				),
             ),
-	        model=VanillaModelConfig(
-	            _target=MipNerfModel,
-	            loss_coefficients={"rgb_loss_coarse": 0.1, "rgb_loss_fine": 1.0},
-	            num_coarse_samples=128,
-	            num_importance_samples=128,
-	            eval_num_rays_per_chunk=1024,
-	        ),
-	    ),
+			model=MyModelConfig(
+            	eval_num_rays_per_chunk=1 << 15,
+				num_nerf_samples_per_ray=128,
+				num_proposal_samples_per_ray=(512, 256),
+				hidden_dim=128,
+				hidden_dim_color=128,
+				appearance_embed_dim=128,
+				base_res=32,
+				max_res=4096,
+				proposal_weights_anneal_max_num_iters=5000,
+				log2_hashmap_size=21,
+			),
+		),
 	    optimizers={
+			"proposal_networks": {
+				"optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+				"scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+			},
 	        "fields": {
 	            "optimizer": RAdamOptimizerConfig(lr=5e-4, eps=1e-08),
-	            "scheduler": None,
-	        }
+	            "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+	        },
+			"encodings": {
+            	"optimizer": AdamOptimizerConfig(lr=0.02),
+            	"scheduler": ExponentialDecaySchedulerConfig(lr_final=0.002, max_steps=30000),
+        	},
 	    },
 	    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
         vis="viewer",
---
+++ my_method/my_pipeline.py
@@ -0,0 +1,62 @@
+import typing
+from dataclasses import dataclass, field
+from typing import Literal, Type
+
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from nerfstudio.models.base_model import ModelConfig
+from nerfstudio.pipelines.base_pipeline import (
+    VanillaPipeline,
+    VanillaPipelineConfig,
+)
+
+from my_method.my_dataManager import MyDataManagerConfig, MyDataManager
+from my_method.my_nerf import MyModelConfig, MyNeRFModel
+
+@dataclass
+class MyPipelineConfig(VanillaPipelineConfig):
+    """Configuration for pipeline instantiation"""
+
+    _target: Type = field(default_factory=lambda: MyPipeline)
+    """target class to instantiate"""
+    datamanager: MyDataManagerConfig() = MyDataManagerConfig()
+    """specifies the datamanager config"""
+    model: MyModelConfig = MyModelConfig()
+    """specifies the model config"""
+
+
+class MyPipeline(VanillaPipeline):
+    def __init__(
+        self,
+        config: MyPipelineConfig,
+        device: str,
+        test_mode: Literal["test", "val", "inference"] = "val",
+        world_size: int = 1,
+        local_rank: int = 0,
+    ):
+        super(VanillaPipeline, self).__init__()
+        self.config = config
+        self.test_mode = test_mode
+        
+        self.datamanager: MyDataManager = config.datamanager.setup(
+            device=device,
+            test_mode=test_mode,
+            world_size=world_size,
+            local_rank=local_rank,
+        )
+        self.datamanager.to(device)
+
+        assert self.datamanager.train_dataset is not None, "Missing input dataset"
+
+        self._model = config.model.setup(
+            scene_box=self.datamanager.train_dataset.scene_box,
+            num_train_data=len(self.datamanager.train_dataset),
+            metadata=self.datamanager.train_dataset.metadata,
+        )
+        self.model.to(device)
+
+        self.world_size = world_size
+        if world_size > 1:
+            self._model = typing.cast(MyNeRFModel, DDP(self._model, device_ids=[local_rank], find_unused_parameters=True))
+            dist.barrier(device_ids=[local_rank])
