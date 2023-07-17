5ec3e38f190fac3e6247297c1be06e2f4008324f
Riley <riley@behlevanas.com>
2023-07-17 21:17:29 +1000

    loss function now based on hazy image

--- my_method/test_nerf.py
+++ my_method/test_nerf.py
@@ -374,7 +374,7 @@ class NerfactoModel(Model):
     def get_loss_dict(self, outputs, batch, metrics_dict=None):
         loss_dict = {}
         image = batch["image"].to(self.device)
-        loss_dict["rgb_loss"] = self.rgb_loss(image, outputs["rgb"])
+        loss_dict["rgb_loss"] = self.rgb_loss(image, outputs["hazy"])
         if self.training:
             loss_dict["interlevel_loss"] = self.config.interlevel_loss_mult * interlevel_loss(
                 outputs["weights_list"], outputs["ray_samples_list"]
